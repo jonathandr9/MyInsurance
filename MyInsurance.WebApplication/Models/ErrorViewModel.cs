1773d070633652ac3da9d3df296e5facff22722b
jonathandr9 <jonathan>
2020-12-13 15:22:00 -0300

    Update Architeture

---
+++ MyInsurance.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyInsurance.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
