ef3460bd6749cf273d78094aef731711a5168b89
jonathandr9 <jonathan>
2020-12-14 02:32:45 -0300

    Details page create

--- MyInsurance.WebApplication/Controllers/OficinaController.cs
+++ MyInsurance.WebApplication/Controllers/OficinaController.cs
@@ -2,7 +2,9 @@
 using Microsoft.Extensions.Logging;
 using MyInsurance.Domain.Models;
 using MyInsurance.Domain.Services;
+using MyInsurance.WebApplication.Dtos;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyInsurance.WebApplication.Controllers
@@ -29,6 +31,18 @@ namespace MyInsurance.WebApplication.Controllers
                  await _oficinaService.ConsultarOficinas(codigoAssociacao, cpfAssociado);
 
             return View("Index",listaOficinasResult);
+
+        }
+
+        public async Task<IActionResult> Detalhes(DetalhesOficinaGet detalhesOficinaGet)
+        {
+            IEnumerable<Oficina> listaOficinasResult =
+                 await _oficinaService.ConsultarOficinas(detalhesOficinaGet.CodigoAssociacao, string.Empty);
+
+            var oficina =
+                listaOficinasResult.FirstOrDefault(a => a.Id == detalhesOficinaGet.Id);
+
+            return View(oficina);
 
         }
     }
---
+++ MyInsurance.WebApplication/Dtos/DetalhesOficinaGet.cs
@@ -0,0 +1,8 @@
+﻿namespace MyInsurance.WebApplication.Dtos
+{
+    public class DetalhesOficinaGet
+    {
+        public int Id { get; set; }
+        public int CodigoAssociacao { get; set; }
+    }
+}
